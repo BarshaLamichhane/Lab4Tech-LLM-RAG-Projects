3e67b29688918341b3d7df6f9b4ff1f055dada91
BarshaLamichhane <barshalamichhane.bl@gmail.com>
2026-05-22 21:25:18 +0200

    edit downloader

--- CV-job-matching-assistant/src/download_model.py
+++ CV-job-matching-assistant/src/download_model.py
@@ -5,11 +5,8 @@ url = "mistral-7b-instruct-v0.1.Q4_0.gguf"  # Replace with the actual URL of the
 
 
 # Path where the model will be saved
-save_path = './models/llama-2-7b-chat.ggmlv3.q2_K.bin'
-### Added later by Barsha starts here ########
-save_path_url_2 = './models/llama-2-7b-chat.Q4_K_M.gguf'
-### Added later by Barsha ends here ########
-# Download the model file
+save_path = f'../../models/{url.split("/")[-1]}'  # Save in the models directory with the same filename
+
 response = requests.get(url, stream=True)
 
 # Check if the download was successful
@@ -19,12 +16,12 @@ if response.status_code == 200:
     #     for chunk in response.iter_content(chunk_size=1024):
     #         if chunk:
     #             f.write(chunk)
-    with open(save_path_url_2, 'wb') as f:
+    with open(save_path, 'wb') as f:
         for chunk in response.iter_content(chunk_size=1024):
             if chunk:
                 f.write(chunk)
 
 
-    print(f'Model downloaded successfully and saved to {save_path_url_2}')
+    print(f'Model downloaded successfully and saved to {save_path}')
 else:
     print('Error downloading the model.')
